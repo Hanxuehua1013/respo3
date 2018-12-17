cb5f857d6006de41fe66f9cb27ec9c002a835194
hanxuehua <hanxuehua_1013@163.com>
2018-12-17 18:40:10 +0800

    添加了一个HellowGit类

---
+++ src/com/itheima/git/HellowGit.java
@@ -0,0 +1,16 @@
+package com.itheima.git;
+
+/**
+ * Created by XueHua
+ * Company:http://www.xuehua.com
+ * Time:2018/12/17 18:32
+ */
+public class HellowGit {
+    public static void main(String[] args) {
+        System.out.println("hellow git");
+        System.out.println("hellow git");
+        System.out.println("hellow git");
+        System.out.println("hellow git");
+        System.out.println("hellow git");
+    }
+}
